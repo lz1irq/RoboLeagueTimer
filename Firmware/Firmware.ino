4447d4c9e9c1f69628c57ba56d0efde476b39543
lz1irq <ivaylo.markoff@gmail.com>
2014-12-27 17:35:13 +0200

    basic command parsing

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -2,13 +2,39 @@
 
 #define TIMES 10
 #define DELAY_MS 10
+#define TOLERANCE 10
+
+#define NEWLINE '\n'  
+
+enum mode{
+  CALIBRATION,
+  STANDBY,
+  WAITING,
+  MEASURING 
+};
+
+struct {
+  char operand[4];
+  int argument;
+} command;
+
+struct {
+  unsigned int near = 0;
+  unsigned int distant = 0;
+  unsigned int tolerance = 10; 
+} 
+threshold;
+
+mode currentMode = CALIBRATION;
 
 void setup() {
   Serial.begin(9600); 
 }
 
 void loop() {
-  Serial.println(sensorAverage()); 
+  if(Serial.available() > 0) {
+    parseCommand(); 
+  }
   delay(100);
 }
 
@@ -19,4 +45,15 @@ int sensorAverage() {
     delay(DELAY_MS); 
   }
   return readings/TIMES;
+}
+
+void parseCommand() {
+  Serial.readBytes(command.operand, 3);
+  Serial.println(command.operand);
+  if(Serial.available() >= 3 ) {
+    char arg[3];
+    Serial.readBytes(arg, Serial.available());
+    command.argument = atoi(arg);
+    Serial.println(command.argument);
+  }
 }
