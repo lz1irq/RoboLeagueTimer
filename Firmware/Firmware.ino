0c1fe28e12e71801d629bcc2006af87701e2bdf9
lz1irq <ivaylo.markoff@gmail.com>
2014-12-27 15:55:44 +0200

    reading average sensor value

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,10 +1,22 @@
 #define SENSOR_PIN A0
 
+#define TIMES 10
+#define DELAY_MS 10
+
 void setup() {
   Serial.begin(9600); 
 }
 
 void loop() {
-  int sensorValue = analogRead(SENSOR_PIN);
-  Serial.println(sensorValue); 
+  Serial.println(sensorAverage()); 
+  delay(100);
+}
+
+int sensorAverage() {
+  int readings = 0;
+  for(int i=0; i<TIMES; i++) {
+    readings += analogRead(SENSOR_PIN);
+    delay(DELAY_MS); 
+  }
+  return readings/TIMES;
 }
